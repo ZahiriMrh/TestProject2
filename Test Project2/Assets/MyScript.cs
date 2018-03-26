8d06d7a793104036dd742321e33e652a9d9ba8f8
DESKTOP-BR6F8PH\Samet <hkarakaya3772@gmail.com>
2018-03-26 14:49:12 +0300

    First Script

---
+++ Test Project2/Assets/MyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Yeni Sürüm deneme");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
